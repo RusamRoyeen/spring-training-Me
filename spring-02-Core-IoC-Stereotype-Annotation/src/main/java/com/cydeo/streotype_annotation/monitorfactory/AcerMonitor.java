59ef7102adbb228c968741dde54bcee6f1924045
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-08 17:53:56 -0400

    AcerMonitor.java is created.

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/monitorfactory/AcerMonitor.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/monitorfactory/AcerMonitor.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Component;
 public class AcerMonitor extends Monitor {
 
     public AcerMonitor() {
-
-        super("25 inch beast","Acer",25);
+        super("25 inch beast", "Acer",25);
     }
 
+    @Override
     public void drawPixelAt() {
-        System.out.println("Drawing pixel with Sony");
+        System.out.println("Drawing pixel with Acer");
     }
 }
+
