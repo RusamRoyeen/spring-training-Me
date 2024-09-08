5c803ee28bd070d691216305b9f554162d513776
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-08 17:54:42 -0400

    PcConfig.java is created.

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.cydeo")
+@ComponentScan("com.cydeo.streotype_annotation")
 public class PcConfig {
+
 }
