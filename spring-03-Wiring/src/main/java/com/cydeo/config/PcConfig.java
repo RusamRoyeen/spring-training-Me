23fa66ec5959bff7f70f68a67b5f99a1dfc46596
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-08 18:18:59 -0400

    Dimensions.java is updated.

--- spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
+++ spring-03-Wiring/src/main/java/com/cydeo/config/PcConfig.java
@@ -8,15 +8,14 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PcConfig {
-
     @Bean
-    public Dimensions dimensions(){
-        return new Dimensions(50,10,10);
+    public Dimensions dimensions() {
+        return new  Dimensions(50, 10, 10);
     }
-
-
     @Bean
     public Case caseDell(Dimensions dimensions){
-        return new DellCase("220B","Dell","240",dimensions);
+
+        return new DellCase("220B", "Dell", "240",dimensions);
     }
+
 }
