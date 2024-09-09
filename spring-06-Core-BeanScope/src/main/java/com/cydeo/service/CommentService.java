bed8d2bb25beb1bed5b5d5dc1f7a201c2de6a8c6
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-09 19:10:31 -0400

    Module 6 is completed.

--- spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
+++ spring-06-Core-BeanScope/src/main/java/com/cydeo/service/CommentService.java
@@ -3,11 +3,13 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
-import com.cydeo.repository.DBCommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
