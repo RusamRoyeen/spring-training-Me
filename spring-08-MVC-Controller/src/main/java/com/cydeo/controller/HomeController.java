f638256792bce5883232609f0dfdcc021e6801f7
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-14 22:02:56 -0400

    HomeController.java is update

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -7,7 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home() {  //which end point? www.amazon.com/login
+    public String getHomePage() {  //which end point? www.amazon.com/login
+        return "home.html";
+    }
+
+    @RequestMapping("/ozzy")
+    public String getHomePage2() {  //which end point? www.amazon.com/lo  gin
+        return "home.html";
+    }
+
+    @RequestMapping//nothing
+    public String getHomePage3() {  //which end point? www.amazon.com/login
+        return "home.html";
+    }
+
+    @RequestMapping({"/apple", "/orange"})
+    public String getHomePage4() {
         return "home.html";
     }
 
