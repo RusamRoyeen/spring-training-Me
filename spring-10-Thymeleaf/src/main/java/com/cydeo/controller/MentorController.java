a93653a4a1bb3b3314c910a2ca0a9facc53f368e
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-18 18:41:40 -0400

    StudentController.java and MentorController.java are updated.

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/mentor")
 public class MentorController {
 
 
         //localhost:8080/register
-        @RequestMapping("/mentor/register")
+        @RequestMapping("/register")
         public String register(){
             return "student/register";
         }
+
+    //localhost:8080/student/drop
+    @RequestMapping("/drop")
+    public String drop() {
+        return "student/register";
+    }
 }
--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,14 +1,25 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping("/student")
 public class StudentController {
-    //localhost:8080/mentor/register
-    @RequestMapping("/student/register")
-    public String register(){
+    //localhost:8080/student/register
+    @RequestMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudents());
+
        return "student/register";
+    }
+
+    //localhost:8080/student/drop
+    @RequestMapping("/drop")
+    public String drop(){
+        return "student/register";
     }
 }
