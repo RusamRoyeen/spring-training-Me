71a41c38c020dadc678af199d70de7c47754f256
Rustam Royeen <rustam.royeen@gmail.com>
2024-09-24 19:01:10 -0400

    Conditional Logic

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,15 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
     //localhost:8080/student/register
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+   @GetMapping("register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudents());
@@ -20,7 +19,8 @@ public class StudentController {
     }
 
     //localhost:8080/student/drop
-    @RequestMapping("/welcome")
+    @RequestMapping(value = "/welcome", method = RequestMethod.POST)
+    @PostMapping("/welcome")
     public String welcome(@RequestParam String name){
 
         return "student/welcome";
